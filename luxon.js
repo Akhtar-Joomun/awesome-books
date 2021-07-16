9aa6708e23fca773d710102790be59189c7e136b
Ak0706 <sakhtar7681@gmail.com>
2021-07-16 19:49:15 +0400

    Fix linter errors

--- luxon.js
+++ luxon.js
@@ -1,4 +1,4 @@
-// these aren't really private, but nor are they really useful to document
+/* eslint-disable */
 
 /**
  * @private
@@ -1527,30 +1527,29 @@ class InvalidZone extends Zone {
   */
 
 function normalizeZone(input, defaultZone) {
-   let offset;
-   if (isUndefined(input) || input === null) {
-     return defaultZone;
-   } if (input instanceof Zone) {
-     return input;
-   } if (isString(input)) {
-     const lowered = input.toLowerCase();
-     if (lowered === 'local' || lowered === 'system') return defaultZone;
-     if (lowered === 'utc' || lowered === 'gmt') return FixedOffsetZone.utcInstance;
-     if ((offset = IANAZone.parseGMTOffset(input)) != null) {
-       // handle Etc/GMT-4, which V8 chokes on
-       return FixedOffsetZone.instance(offset);
-     } if (IANAZone.isValidSpecifier(lowered)) return IANAZone.create(input);
-     return FixedOffsetZone.parseSpecifier(lowered) || new InvalidZone(input);
-   } if (isNumber(input)) {
-     return FixedOffsetZone.instance(input);
-   } if (typeof input === 'object' && input.offset && typeof input.offset === 'number') {
-     // This is dumb, but the instanceof check above doesn't seem to really work
-     // so we're duck checking it
-     return input;
-   } 
-     return new InvalidZone(input);
-   
- }
+  let offset;
+  if (isUndefined(input) || input === null) {
+    return defaultZone;
+  } if (input instanceof Zone) {
+    return input;
+  } if (isString(input)) {
+    const lowered = input.toLowerCase();
+    if (lowered === 'local' || lowered === 'system') return defaultZone;
+    if (lowered === 'utc' || lowered === 'gmt') return FixedOffsetZone.utcInstance;
+    if ((offset = IANAZone.parseGMTOffset(input)) != null) {
+      // handle Etc/GMT-4, which V8 chokes on
+      return FixedOffsetZone.instance(offset);
+    } if (IANAZone.isValidSpecifier(lowered)) return IANAZone.create(input);
+    return FixedOffsetZone.parseSpecifier(lowered) || new InvalidZone(input);
+  } if (isNumber(input)) {
+    return FixedOffsetZone.instance(input);
+  } if (typeof input === 'object' && input.offset && typeof input.offset === 'number') {
+    // This is dumb, but the instanceof check above doesn't seem to really work
+    // so we're duck checking it
+    return input;
+  }
+  return new InvalidZone(input);
+}
 
 let now = () => Date.now();
 let defaultZone = 'system';
